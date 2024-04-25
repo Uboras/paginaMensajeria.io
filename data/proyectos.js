4f4a83eb9252f56dfc4ceb6df329842389f2774e
Uboras <129528183+Uboras@users.noreply.github.com>
2024-04-25 16:41:51 -0300

    modificacion en media

---
+++ data/proyectos.js
@@ -0,0 +1,48 @@
+const proyectos = {
+  proyectos: [
+    {
+      nombre: "Calculadora",
+      tecnologias: "Html, CSS y JS",
+      Descripcion: "",
+      link: "",
+    },
+    {
+      nombre: "LogIn",
+      tecnologias: "React Native",
+      Descripcion: "",
+      link: "",
+    },
+    {
+      nombre: "10 Programas en java",
+      tecnologias: "Java",
+      Descripcion: "",
+      link: "",
+    },
+    {
+      nombre: "Calculadora en java",
+      tecnologias: "Java",
+      Descripcion: "",
+      link: "",
+    },
+    {
+      nombre: "UTN DesarrolladorWeb",
+      tecnologias: "HTML,CSS Y REACT",
+      Descripcion: "",
+      link: "",
+    },
+    {
+      nombre: "Egg Corporation PseudoCódigo",
+      tecnologias: "PsInt,  java",
+      Descripcion: "",
+      link: "",
+    },
+    {
+      nombre: "Codo a Codo FullStack Python/Vue",
+      tecnologias: "HTML, CSS Y JS-VUE",
+      Descripcion: "",
+      link: "",
+    },
+  ],
+};
+
+export default proyectos;
